e9ec07f8d7874b6536553ca0111a94f7426bdba1
Angelinne <kayumi85@hotmail.com>
2023-03-02 16:14:14 +0900

    fix main

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -2,7 +2,7 @@
 
 void Span::addNumber(int num)
 {
-	if (_SpanSize == MaxArraySize)
+	if (_SpanSize == this->MaxArraySize)
 		throw std::runtime_error("ERROR");
 	this->_array[_SpanSize] = num;
 	_SpanSize++;
@@ -11,7 +11,7 @@ void Span::addNumber(int num)
 int Span::shortestSpan()
 {
 	if (_SpanSize < 2)
-		throw std::runtime_error("TOO SMAL SIZE");
+		throw std::runtime_error("TOO SMAL SIZE 01");
 	std::sort(_array, _array + _SpanSize);
 	int shortest = _array[1] - _array[0];
 	for (unsigned int i = 2; i < _SpanSize; i++)
@@ -25,7 +25,7 @@ int Span::shortestSpan()
 int Span::longestSpan()
 {
 	if (_SpanSize < 2)
-		throw std::runtime_error("TOO SMAL SIZE");
+		throw std::runtime_error("TOO SMAL SIZE 02");
 	int min = *std::min_element(_array, _array + _SpanSize);
 	int max = *std::max_element(_array, _array + _SpanSize);
 	return(max - min);
@@ -35,7 +35,7 @@ Span::Span(unsigned int N)
 {
 	this->MaxArraySize = N;
 	this->_SpanSize = 0;
-	this->_array = new int (this->MaxArraySize);
+	this->_array = new int [this->MaxArraySize];
 }
 
 Span::~Span()
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,23 +2,37 @@
 
 int main()
 {
-	std::cout << "=======Normal Test=========" << std::endl;
-	Span sp = Span(5);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-
-	std::cout << "=======Crazy Test =========" << std::endl;
-	Span sp2 = Span(10001);
-	for (size_t i = 1; i < 10000; i++)
+	try
 	{
-		sp2.addNumber(i);
+		std::cout << "=======Normal Test=========" << std::endl;
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+
+		std::cout << "=======Crazy Test =========" << std::endl;
+		Span sp2 = Span(10001);
+		for (size_t i = 1; i < 10000; i++)
+		{
+			sp2.addNumber(i);
+		}
+		std::cout << sp2.shortestSpan() << std::endl;
+		std::cout << sp2.longestSpan() << std::endl;
+
+		std::cout << "=======Error Test =========" << std::endl;
+		Span sp3 = Span(1);
+		sp3.addNumber(1);
+		std::cout << sp3.shortestSpan() << std::endl;
+		std::cout << sp3.longestSpan() << std::endl;
 	}
-	std::cout << sp2.shortestSpan() << std::endl;
-	std::cout << sp2.longestSpan() << std::endl;
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 	return 0;
 }
